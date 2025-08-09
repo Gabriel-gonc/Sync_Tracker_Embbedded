c1118241ada5557b6836effc503e06dff8c196df
Gabriel <mendgabriel.furnas@outlook.com>
2025-08-08 21:12:37 -0300

    * Correta acumulação e envio dos dados. * Implementada a função para tratar as mensagens de controle. * Início da implementação da máquina de estados. * Organizado o project_types e deixando o numero de ciclos para diferença de pulsos referenciado no sensor, o que de fato ocorrerá. Desse modo, a frequência do sensor é quem dita quantas amostras teremos por segundo (30 amostras se não houver a compensação de imãs com pares de polos)

--- components/project_types/include/project_types.h
+++ components/project_types/include/project_types.h
@@ -8,19 +8,25 @@
 #define ELETRIC_GRID_PIN GPIO_NUM_5
 #define SENSOR_PIN GPIO_NUM_4
 
-#define ESP_INTR_FLAG_DEFAULT 0
-
 /** @brief Project Constants */
-#define PARES_POLOS 2
-#define NUM_CYCLES_FREQ_GRID 60
-#define NUM_CYCLES_FREQ_SENSOR (NUM_CYCLES_FREQ_GRID / PARES_POLOS)
-#define NUM_CYCLES_DIFF_PULSE NUM_CYCLES_FREQ_GRID * 10
-#define GRID_WINDOW_FILTER 15000 // 11ms
-#define SENSOR_WINDOW_FILTER (GRID_WINDOW_FILTER * PARES_POLOS)
+#define POLES_PAIRS (2) // If the machine has 1 magnet for each pole pair this value must be 1
+#define GRID_FREQUENCY (60)
+#define SENSOR_FREQUENCY (GRID_FREQUENCY / POLES_PAIRS)
+#define SAMPLE_DURATION_SEC (1)
+#define NUM_CYCLES_DIFF_PULSE (SENSOR_FREQUENCY * SAMPLE_DURATION_SEC) 
+#define GRID_WINDOW_FILTER (15000) 
+#define SENSOR_WINDOW_FILTER (GRID_WINDOW_FILTER * POLES_PAIRS)
 
 /** @brief UDP Constants */
 #define UDP_RX_BUFFER_SIZE 50
 #define UDP_TX_BUFFER_SIZE NUM_CYCLES_DIFF_PULSE * 6
+
+/** @brief UDP Messages and Commands */
+#define MSG_SYNC "SYNC"
+#define CMD_ACK "ACK"
+#define CMD_NACK "NACK"
+#define MSG_MON "MONITORING"
+#define MSG_OP "OPERATIONAL"
 
 /***********************************************************
  * Project Types
@@ -31,6 +37,15 @@ typedef enum {
     SYSTEM_WIFI_DISCONNECTED,
     SYSTEM_UDP_CREATED,
 } system_event_t;
+
+/** @brief System States */
+typedef enum {
+    STATE_IDLE,
+    STATE_CONNECTED,
+    STATE_MONITORING,
+    STATE_OPERATIONAL,
+    STATE_ERROR,
+} system_state_t;
 
 /** @brief System callback. */
 typedef void (*system_callback_t)(system_event_t event);
--- main/main.c
+++ main/main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <inttypes.h>
+#include <math.h>
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -19,93 +20,31 @@
 /***********************************************************
  * Defines
 ***********************************************************/
-#define TAG "MAIN"
-#define SOFTWARE_DATA_ANALISYS 1
-
-#define MSG_SYNC "SYNC"
-#define CMD_ACK "ACK"
+#define MAIN_TAG "MAIN"
 
 /***********************************************************
  * Function Prototypes
 ***********************************************************/
-static void IRAM_ATTR grid_itr_callback(void *arg);
-static void IRAM_ATTR sensor_itr_callback(void *arg);
-static void gpio_init(void);
-void system_callback(system_event_t event);
-void grid_freq_task(void *pvParameters);
-void sensor_freq_task(void *pvParameters);
-void measure_pulse_difference(void *pvParameters);
+static void system_callback(system_event_t event);
+static system_state_t trait_messages(bool hand_shaking, bool state_comp);
+static void time_difference_mock();
 
 /***********************************************************
  * Variables
 ***********************************************************/
-/** @brief Variáveis para armazenar a diferença de tempo entre os pulsos da rede. */
-volatile uint32_t delta_time_freq_grid = 0;
-
-/** @brief Variáveis para armazenar a diferença de tempo entre os pulsos do sensor. */
-volatile uint32_t delta_time_freq_sensor = 0;
-
-/** @brief Flag para controlar quando a amostragem da rede é feita. */
-volatile bool measurement_ready_freq_grid = false;
-
-/** @brief Flag para controlar quando a amostragem do sensor é feita. */
-volatile bool measurement_ready_freq_sensor = false;
-
-volatile bool measurement_ready_diff_pulse = false;
-
-/** @brief Variáveis para armazenar o instante de último pulso da rede. */
-volatile uint32_t T_firstpulse_freq_grid = 0;
-
-/** @brief Variáveis para armazenar o instante de último pulso do sensor. */
-volatile uint32_t T_firstpulse_freq_sensor = 0;
-
-/** @brief Variáveis para armazenar o valor das frequências cálculadas. */
-double Freq_pulse_grid = 0;
-
-/** @brief Variáveis para armazenar o valor das frequências cálculadas. */
-double Freq_pulse_sensor = 0;
-
-/** @brief Variáveis para fazer o cálculo da média de frequência da rede. */
-volatile int cont_ciclos_freq_grid = 0;
-uint32_t somatoria_tempos_freq_grid = 0;
-
-/** @brief Variáveis para fazer o cálculo da média de frequência do sensor. */
-volatile int cont_ciclos_freq_sensor = 0;
-uint32_t somatoria_tempos_freq_sensor = 0;
-
-volatile int cont_ciclos_diff_pulse = 0;
-
-/** @brief Array to store grid signal periods. */
-volatile uint32_t time_array_freq_grid[NUM_CYCLES_FREQ_GRID];
-
-/** @brief Array to store sensor signal periods. */
-volatile uint32_t time_array_freq_sensor[NUM_CYCLES_FREQ_SENSOR];
-
-volatile uint32_t time_array_delta_pulse[NUM_CYCLES_DIFF_PULSE]; 
-
-/** @brief Var to sign grid first measure cycle triggered. */
-volatile bool triggered_process_freq_grid = false;
-
-/** @brief Var to sign sensor first measure cycle triggered. */
-volatile bool triggered_process_freq_sensor = false;
-
-volatile bool sensor_pulse_ready = false;
-
-uint8_t estado = 0;
-enum {calibracao, operacao};
+/** @brief Array to store the time difference between sensor and grid pulses */
+static volatile uint16_t time_difference[NUM_CYCLES_DIFF_PULSE] = {0};
 
 /** @brief Event Group variables. */
-EventGroupHandle_t main_event_group;
+EventGroupHandle_t main_event_group = NULL;
 const int WIFI_CONNECTED_BIT = BIT0;
-const int DELTA_PULSE_MEASUREMENT_READY_BIT = BIT1;
 
 /** @brief Buffers for udp socket. */
-char udp_receive_buffer[UDP_RX_BUFFER_SIZE];
-char udp_send_buffer[UDP_TX_BUFFER_SIZE];
+char udp_receive_buffer[UDP_RX_BUFFER_SIZE] = {0};
+char udp_send_buffer[UDP_TX_BUFFER_SIZE] = {0};
 
-SemaphoreHandle_t udp_semaphore = NULL;
-
-volatile uint32_t ref_delta_pulse = 0;
+/** @brief Variable to store current System State */
+static volatile system_state_t current_state = STATE_IDLE;
 
 /***********************************************************
  * Main Function
@@ -114,9 +53,6 @@ void app_main(void)
 {
     /* Create event group */
     main_event_group = xEventGroupCreate();
-
-    /* Create Semaphore */
-    udp_semaphore = xSemaphoreCreateMutex();
 
     /* Init wifi*/
     wifi_init_softap();
@@ -127,29 +63,21 @@ void app_main(void)
     udp_socket_init();
 
     /* Establishing connection */
-    while(1){
-        if (udp_socket_receive(udp_receive_buffer, sizeof(udp_receive_buffer)) > 0)
-        {
-            if (strncmp(udp_receive_buffer, MSG_SYNC, strlen(MSG_SYNC)) == 0)
-            {
-                int len = snprintf(udp_send_buffer, sizeof(udp_send_buffer), CMD_ACK);
-                udp_socket_send(udp_send_buffer, len);
-                vTaskDelay(2000 / portTICK_PERIOD_MS);
-                break;
-            }
-        }
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    current_state = trait_messages(true, false);
+    if (current_state != STATE_CONNECTED)
+    {
+        ESP_LOGE(MAIN_TAG, "Failed to establish connection!");
+        esp_restart();
     }
 
-    /* Initializing gpio. */
-    gpio_init();
-
-    /* Task Create */
-    xTaskCreate(measure_pulse_difference, "measure_pulse_difference", 4096, NULL, 0, NULL);
-#ifndef SOFTWARE_DATA_ANALISYS
-    xTaskCreate(grid_freq_task, "grid_freq_task", 2048, NULL, 0, NULL);
-    xTaskCreate(sensor_freq_task, "sensor_freq_task", 2048, NULL, 0, NULL);
-#endif
+    /* Receiving next state command */
+    current_state = trait_messages(false, true);
+    
+    /* Mocking monitoring data */
+    if (current_state == STATE_MONITORING)
+    {
+        time_difference_mock();
+    }
 
     /* Main loop. */
     while(true)
@@ -161,15 +89,16 @@ void app_main(void)
 /***********************************************************
  * System Callback
 ***********************************************************/
-void system_callback(system_event_t event)
+/** @brief Function to enable inter-components communication */
+static void system_callback(system_event_t event)
 {
     switch (event) {
         case SYSTEM_WIFI_CONNECTED:
-            // ESP_LOGI(TAG, "WiFi connected");
+            // ESP_LOGI(MAIN_TAG, "WiFi connected");
             xEventGroupSetBits(main_event_group, WIFI_CONNECTED_BIT);
             break;
         case SYSTEM_WIFI_DISCONNECTED:
-            // ESP_LOGI(TAG, "WiFi disconnected");
+            esp_restart();
             break;
         default:
             break;
@@ -177,205 +106,97 @@ void system_callback(system_event_t event)
 }
 
 /***********************************************************
- * Funções
+ * Functions
 ***********************************************************/
-static void gpio_init(void)
-{   
-    /* Config for Grid Signal */
-    gpio_config_t io_conf;
-    io_conf.intr_type = GPIO_INTR_NEGEDGE; //interrupt of rising edge
-    io_conf.mode = GPIO_MODE_INPUT;
-    io_conf.pin_bit_mask = (1ULL<<ELETRIC_GRID_PIN);
-    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
-    io_conf.pull_up_en = GPIO_PULLUP_ENABLE;
+/** @brief Function to receive, trait and send udp control messages
+ * 
+ * @param hand_shaking True for hand-shaking process, false otherwise
+ * @param state_comp True for state setting process, false otherwise
+ */
+static system_state_t trait_messages(bool hand_shaking, bool state_comp)
+{
+    while (1)
+    {
+        int bytes_received = udp_socket_receive(udp_receive_buffer, sizeof(udp_receive_buffer));
+        if (bytes_received > 0)
+        {
+            if (hand_shaking && !state_comp)
+            {
+                if (strncmp(udp_receive_buffer, MSG_SYNC, strlen(MSG_SYNC)) == 0)
+                {
+                    int len = snprintf(udp_send_buffer, sizeof(udp_send_buffer), "%s", CMD_ACK);
+                    udp_socket_send(udp_send_buffer, len);
+                    ESP_LOGI(MAIN_TAG, "Connected!");
+                    return STATE_CONNECTED;  
+                }
+                else 
+                {
+                    int len = snprintf(udp_send_buffer, sizeof(udp_send_buffer), "%s", CMD_NACK);
+                    udp_socket_send(udp_send_buffer, len);
+                }
+            }
 
-    gpio_config(&io_conf); //configure GPIO with the given settings
-    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT); //install gpio isr service
-    gpio_isr_handler_add(ELETRIC_GRID_PIN, grid_itr_callback, NULL); //hook isr handler for specific gpio pin
+            if (state_comp && !hand_shaking)
+            {
+                if (strncmp(udp_receive_buffer, MSG_MON, strlen(MSG_MON)) == 0)
+                {
+                    ESP_LOGI(MAIN_TAG, "Received monitoring command");
+                    int len = snprintf(udp_send_buffer, sizeof(udp_send_buffer), "%s", CMD_ACK);
+                    udp_socket_send(udp_send_buffer, len);
+                    return STATE_MONITORING;
+                }
+                else if (strncmp(udp_receive_buffer, MSG_OP, strlen(MSG_OP)) == 0)
+                {
+                    ESP_LOGI(MAIN_TAG, "Received operational command");
+                    int len = snprintf(udp_send_buffer, sizeof(udp_send_buffer), "%s", CMD_ACK);
+                    udp_socket_send(udp_send_buffer, len);
+                    return STATE_OPERATIONAL;
+                }
+                else 
+                {
+                    ESP_LOGW(MAIN_TAG, "Invalid command received: %s", udp_receive_buffer);
+                    int len = snprintf(udp_send_buffer, sizeof(udp_send_buffer), "%s", CMD_NACK);
+                    udp_socket_send(udp_send_buffer, len);
+                }
+            }
+        }
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
 
-    /* Config for Sensor Signal */
-    io_conf.intr_type = GPIO_INTR_NEGEDGE; //interrupt of rising edge
-    io_conf.mode = GPIO_MODE_INPUT;
-    io_conf.pin_bit_mask = (1ULL<<SENSOR_PIN);
-    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
-    io_conf.pull_up_en = GPIO_PULLUP_ENABLE;
+/** @brief Funtion to mock measure of the pulse difference time */
+static void time_difference_mock()
+{
+    uint16_t delay_time = (uint16_t)((NUM_CYCLES_DIFF_PULSE / SENSOR_FREQUENCY) * 1000);
 
-    gpio_config(&io_conf); //configure GPIO with the given settings
-    gpio_isr_handler_add(SENSOR_PIN, sensor_itr_callback, NULL); // hook isr handler for specific gpio pin
+    while (true)
+    {
+        for (uint16_t i = 0; i < NUM_CYCLES_DIFF_PULSE; i++)
+        {
+            time_difference[i] = 10000 + 100 * i; 
+        }
+
+        /* Simulate grid sampling delay */
+        vTaskDelay(delay_time / portTICK_PERIOD_MS);
+
+        uint16_t pos = 0;
+        for (uint16_t i = 0; i < NUM_CYCLES_DIFF_PULSE; i++)
+        {
+            int len = snprintf((udp_send_buffer + pos), (sizeof(udp_send_buffer) - pos), "%u,", time_difference[i]);
+            pos += len;
+        }
+
+        /* Avoid send the null terminator. */
+        pos = pos - 1;
+        udp_socket_send(udp_send_buffer, pos);
+    }
 }
 
 /***********************************************************
  * FreeRTOS Tasks
 ***********************************************************/
-void grid_freq_task(void *pvParameters){
 
-    while(true)
-    {
-        if (measurement_ready_freq_grid){
-
-            /* Cálculo da frequência média. */
-            somatoria_tempos_freq_grid = 0;
-            for (int i = 0; i < NUM_CYCLES_FREQ_GRID; i++){
-                somatoria_tempos_freq_grid += time_array_freq_grid[i];
-            }
-            double media_tempos = somatoria_tempos_freq_grid * 1.0 / NUM_CYCLES_FREQ_GRID;
-            Freq_pulse_grid = 1.0 / (media_tempos / 1000000);
-
-            /* Formação da string de dados. */
-            xSemaphoreTake(udp_semaphore, portMAX_DELAY);
-            memset(udp_send_buffer, 0, sizeof(udp_send_buffer));
-            int len = snprintf(udp_send_buffer, sizeof(udp_send_buffer), "\nFrequência Rede: %.5f Hz", Freq_pulse_grid);
-
-            /* Envio da frequência via UDP. */
-            udp_socket_send(udp_send_buffer, len);
-            xSemaphoreGive(udp_semaphore);
-
-            /* Resetando variáveis para o próximo ciclo de medição. */
-            measurement_ready_freq_grid = false;
-            cont_ciclos_freq_grid = 0;
-        }
-        vTaskDelay(100 / portTICK_PERIOD_MS);
-    }
-}
-
-void sensor_freq_task(void *pvParameters){
-
-    while(true)
-    {
-        if (measurement_ready_freq_sensor){
-
-            /* Cálculo da frequência média. */
-            somatoria_tempos_freq_sensor = 0;
-            for (int i = 0; i < NUM_CYCLES_FREQ_SENSOR; i++){
-                somatoria_tempos_freq_sensor += time_array_freq_sensor[i];
-            }
-            double media_tempos = somatoria_tempos_freq_sensor * 1.0 / NUM_CYCLES_FREQ_SENSOR;
-            Freq_pulse_sensor = 1.0 / (media_tempos / 1000000);
-
-            /* Formação da string de dados. */
-            xSemaphoreTake(udp_semaphore, portMAX_DELAY);
-            memset(udp_send_buffer, 0, sizeof(udp_send_buffer));
-            int len = snprintf(udp_send_buffer, sizeof(udp_send_buffer), "\nFrequência Sens: %.5f Hz", Freq_pulse_sensor);
-
-            /* Envio da frequência via UDP. */
-            udp_socket_send(udp_send_buffer, len);
-            xSemaphoreGive(udp_semaphore);
-
-            /* Resetando variáveis para o próximo ciclo de medição. */
-            measurement_ready_freq_sensor = false;
-            cont_ciclos_freq_sensor = 0;
-        }
-        vTaskDelay(100 / portTICK_PERIOD_MS);
-    }
-}
-
-void measure_pulse_difference(void *pvParameters)
-{
-
-    while(true)
-    {
-        if (measurement_ready_diff_pulse){
-
-            /* Cálculo da frequência média. */
-            xSemaphoreTake(udp_semaphore, portMAX_DELAY);
-            for (int i = 0; i < NUM_CYCLES_DIFF_PULSE; i++){
-                
-                /* Formação da string de dados. */
-                memset(udp_send_buffer, 0, sizeof(udp_send_buffer));
-#ifndef SOFTWARE_DATA_ANALISYS
-                int len = snprintf(udp_send_buffer, sizeof(udp_send_buffer), "\nDelta_pulse[%d] = %ld",
-                                         i, time_array_delta_pulse[i]);
-#else           
-                int len = snprintf(udp_send_buffer, sizeof(udp_send_buffer), "%ld", time_array_delta_pulse[i]);
-#endif
-
-                udp_socket_send(udp_send_buffer, len);
-            }
-            xSemaphoreGive(udp_semaphore);
-            
-            /* Resetando variáveis para o próximo ciclo de medição. */
-            measurement_ready_diff_pulse = false;
-            cont_ciclos_diff_pulse = 0;
-        }
-        vTaskDelay(100 / portTICK_PERIOD_MS);
-    }
-}
 
 /***********************************************************
  * Callbacks for GPIO Interrupts
 ***********************************************************/
-static void IRAM_ATTR grid_itr_callback(void *arg)
-{
-    if (!triggered_process_freq_grid)
-    {
-        T_firstpulse_freq_grid = esp_timer_get_time();
-        triggered_process_freq_grid = true;
-    }
-    else 
-    {
-        delta_time_freq_grid = esp_timer_get_time() - T_firstpulse_freq_grid;
-        if (delta_time_freq_grid > GRID_WINDOW_FILTER) 
-        {
-            T_firstpulse_freq_grid = esp_timer_get_time();
-
-            /* Permitir o cálculo da frequência a cada segundo sem interromper o processo. */
-            if (!measurement_ready_freq_grid)
-            {
-                time_array_freq_grid[cont_ciclos_freq_grid] = delta_time_freq_grid;
-                cont_ciclos_freq_grid++;
-                if (cont_ciclos_freq_grid >= (NUM_CYCLES_FREQ_GRID)){
-                    measurement_ready_freq_grid = true;
-                }
-            }
-
-            /* Fazer o cálculo de sincronismo em cada ciclo. */
-            if (sensor_pulse_ready)
-            {
-                //TODO: disparo do alarme ou proteção em caso de perda de sincronismo.
-                
-                if (!measurement_ready_diff_pulse)
-                {
-                    time_array_delta_pulse[cont_ciclos_diff_pulse] = T_firstpulse_freq_grid - ref_delta_pulse;
-                    cont_ciclos_diff_pulse++;
-                    if (cont_ciclos_diff_pulse >= (NUM_CYCLES_DIFF_PULSE)){
-                        measurement_ready_diff_pulse = true;
-                    }
-                }
-                sensor_pulse_ready = false;
-            }
-        }
-    }
-}
-
-static void IRAM_ATTR sensor_itr_callback(void *arg)
-{
-    if (!triggered_process_freq_sensor)
-    {
-        T_firstpulse_freq_sensor = esp_timer_get_time();
-        triggered_process_freq_sensor = true;
-    }
-    else 
-    {
-        delta_time_freq_sensor = esp_timer_get_time() - T_firstpulse_freq_sensor;
-        if (delta_time_freq_sensor > SENSOR_WINDOW_FILTER)
-        {
-            T_firstpulse_freq_sensor = esp_timer_get_time();
-
-            /* Permitir o cálculo da frequência a cada segundo sem interromper o processo. */
-            if (!measurement_ready_freq_sensor)
-            {
-                time_array_freq_sensor[cont_ciclos_freq_sensor] = delta_time_freq_sensor;
-                cont_ciclos_freq_sensor++;
-                if (cont_ciclos_freq_sensor >= (NUM_CYCLES_FREQ_SENSOR)){
-                    measurement_ready_freq_sensor = true;
-                }
-            }
-
-            /* Fazer o cálculo de sincronismo em cada ciclo. */
-            if (!sensor_pulse_ready){
-                sensor_pulse_ready = true;
-                ref_delta_pulse = T_firstpulse_freq_sensor;
-            }
-        }
-    }
-}
